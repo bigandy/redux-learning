babeee6e027a4cecf56350adbe4087747f1ff389
Andrew Hudson <ahudson@gmail.com>
2017-02-27 16:51:58 +0000

    start on comments

--- client/components/Single.js
+++ client/components/Single.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
+import Photo from './Photo';
+import Comments from './Comments';
+
 export default class Single extends React.Component {
     render() {
+
+		// index of the post
+		const i = this.props.posts.findIndex(post => post.code === this.props.params.postId);
+
+		// get us the post
+        const post = this.props.posts[i];
+
+        console.log(i, post);
+
         return (
             <div className="single-photo">
-                I'm the single
+                <Photo i={ i } post={ post} { ...this.props } />
+                <Comments />
             </div>
         );
     }
